c42a6bc624096b3ce1a153cd7a80176862c29cfd
Yelysei <49418963+Chief1y@users.noreply.github.com>
2023-08-01 03:06:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ switcher.addEventListener('change', () => {
 
 function start() {
     gapi.client.init({
-        'apiKey': 'AIzaSyAD2ItgU1H5M7WXBXV68TLn1ncON0vM45A',
+        'apiKey': 'APINEEDED',//change to real api
         'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
     }).then(function() {
         return gapi .client.youtube.playlistItems.list({
